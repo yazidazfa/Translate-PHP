aaf7b6428299c2c25716bff06db97855f8af6973
yazidazfa <yasidafa@gmail.com>
2023-12-05 15:20:39 +0700

    first commit

---
+++ Translet2/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Translet2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.btn_translate = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(330, 12);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_browse.TabIndex = 0;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // btn_translate
+            // 
+            this.btn_translate.Location = new System.Drawing.Point(411, 12);
+            this.btn_translate.Name = "btn_translate";
+            this.btn_translate.Size = new System.Drawing.Size(75, 23);
+            this.btn_translate.TabIndex = 1;
+            this.btn_translate.Text = "Translate";
+            this.btn_translate.UseVisualStyleBackColor = true;
+            this.btn_translate.Click += new System.EventHandler(this.btn_translate_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(492, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(833, 436);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(825, 410);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Input";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(825, 407);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Output";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(819, 401);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(819, 404);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 489);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_translate);
+            this.Controls.Add(this.btn_browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.Button btn_translate;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Translet2/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translet2
+{
+    public partial class Form1 : Form
+    {
+        private readonly StringBuilder sourceCodeBuilder;
+        private string selectedJsonFilePath;
+        public Form1()
+        {
+            InitializeComponent();
+            sourceCodeBuilder = new StringBuilder();
+        }
+
+        private void GeneratePhpCode(string jsonFilePath)
+        {
+            // Read JSON file content
+            string umlDiagramJson = File.ReadAllText(jsonFilePath);
+
+            // Decode JSON data
+            JsonData json = JsonConvert.DeserializeObject<JsonData>(umlDiagramJson);
+
+            // Generate PHP code
+            GenerateNamespace(json.sub_name);
+
+            foreach (var model in json.model)
+            {
+                if (model.type == "class")
+                {
+                    GenerateClass(model);
+                }
+                else if (model.type == "association" && model.model != null)
+                {
+                    GenerateAssociationClass(model.model);
+                }
+            }
+
+            // Display or save the generated PHP code
+            richTextBox2.Text = sourceCodeBuilder.ToString();
+        }
+
+        private void GenerateNamespace(string namespaceName)
+        {
+            sourceCodeBuilder.AppendLine($"<?php\nnamespace {namespaceName}\n");
+        }
+
+        private void GenerateClass(JsonData.Model model)
+        {
+            sourceCodeBuilder.AppendLine($"class {model.class_name} {{");
+
+            foreach (var attribute in model.attributes)
+            {
+                GenerateAttribute(attribute);
+            }
+
+            sourceCodeBuilder.AppendLine("");
+
+            foreach (var status in model.attributes)
+            {
+                GenerateState(status);
+            }
+
+            sourceCodeBuilder.AppendLine("");
+
+            if (model.states != null)
+            {
+                foreach (var state in model.states)
+                {
+                    GenerateStateTransitionMethod(state);
+                }
+            }
+
+            if (model.states != null)
+            {
+                GenerateGetState();
+            }
+
+
+            sourceCodeBuilder.AppendLine("}\n\n");
+        }
+
+        private void GenerateAttribute(JsonData.Attribute1 attribute)
+        {
+            // Adjust data types as needed
+            string dataType = MapDataType(attribute.data_type);
+
+            if (dataType != "state") 
+            {
+                sourceCodeBuilder.AppendLine($"    private {dataType} ${attribute.attribute_name};");
+            }
+        }
+
+        private void GenerateAssociationClass(JsonData.Model associationModel)
+        {
+            // Check if associationModel is not null
+            if (associationModel == null)
+            {
+                // Handle the case where associationModel is null, e.g., throw an exception or log a message
+                return;
+            }
+
+            sourceCodeBuilder.AppendLine($"class {associationModel.class_name} {{");
+
+            foreach (var attribute in associationModel.attributes)
+            {
+                // Adjust data types as needed
+                string dataType = MapDataType(attribute.data_type);
+
+                sourceCodeBuilder.AppendLine($"    private {dataType} ${attribute.attribute_name};");
+            }
+
+            // Check if associatedClass.@class is not null before iterating
+            if (associationModel.@class != null)
+            {
+                foreach (var associatedClass in associationModel.@class)
+                {
+                    if (associatedClass.class_multiplicity == "1..1")
+                    {
+                        sourceCodeBuilder.AppendLine($"    private {associatedClass.class_name} ${associatedClass.class_name};");
+                    }
+                    else
+                    {
+                        sourceCodeBuilder.AppendLine($"    private array ${associatedClass.class_name}List;");
+                    }
+                }
+            }
+
+            sourceCodeBuilder.AppendLine("}\n\n");
+        }
+
+        private void GenerateState(JsonData.Attribute1 status) 
+        {
+            if (status.attribute_name == "status")
+            {
+                sourceCodeBuilder.AppendLine("     private $state;");
+            }
+        }
+        
+        private void GenerateStateTransitionMethod(JsonData.State state)
+        {
+            sourceCodeBuilder.AppendLine($"     public function {state.state_name}() {{");
+            sourceCodeBuilder.AppendLine($"       $this->state = '{state.state_name}';");
+            sourceCodeBuilder.AppendLine($"       echo \"state berubah menjadi {state.state_name}\";");
+            sourceCodeBuilder.AppendLine($"}}\n");
+        }
+
+        private void GenerateGetState()
+        {
+            sourceCodeBuilder.AppendLine($"     public function GetState() {{");
+            sourceCodeBuilder.AppendLine($"       $this->state;");
+            sourceCodeBuilder.AppendLine($"}}\n");
+        }
+        public class JsonData
+        {
+            public string sub_name { get; set; }
+            public List<Model> model { get; set; }
+
+            public class Model
+            {
+                public string type { get; set; }
+                public string class_name { get; set; }
+                public List<Attribute1> attributes { get; set; }
+                public List<State> states { get; set; }
+                public Model model { get; set; }
+                public List<Class1> @class { get; set; }
+            }
+
+            public class Attribute1
+            {
+                public string attribute_name { get; set; }
+                public string data_type { get; set; }
+                public string default_value { get; set; }
+                public string attribute_type { get; set; }
+            }
+
+            public class State
+            {
+                public string state_name { get; set; }
+            }
+
+            public class Class1
+            {
+                public string class_name { get; set; }
+                public string class_multiplicity { get; set; }
+                public List<Attribute> attributes { get; set; }
+                public List<Class1> @class { get; set; }
+            }
+
+            public class Attribute
+            {
+                public string attribute_name { get; set; }
+                public string data_type { get; set; }
+                public string attribute_type { get; set; }
+            }
+        }
+
+        private string MapDataType(string dataType)
+        {
+            switch (dataType.ToLower())
+            {
+                case "integer":
+                    return "int";
+                case "id":
+                    return "int";
+                case "string":
+                    return "string";
+                case "bool":
+                    return "bool";
+                case "real":
+                    return "float";
+                // Add more mappings as needed
+                default:
+                    return dataType; // For unknown types, just pass through
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Open Json Diagram File";
+            dialog.Filter = "Json Diagram Files|*.json";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedJsonFilePath = dialog.FileName;
+                string displayJson = File.ReadAllText(selectedJsonFilePath);
+                tabControl1.SelectTab(tabPage1);
+                richTextBox1.Text = displayJson;
+            }
+        }
+
+        private void btn_translate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(selectedJsonFilePath) && File.Exists(selectedJsonFilePath))
+                {
+                    GeneratePhpCode(selectedJsonFilePath);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a valid JSON file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating PHP code: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
